59096531a33686d9df7021be6d07bdee6887b947
Hryhoriy <hryhoriy.bilous@gmail.com>
2024-03-17 17:23:31 +0200

    fixed .env.sample and secret_key added to .env

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$wpp*666d1xq-)*!sg#=et4=$gcf6n)hitvp^4ks+-8c@ihf$0"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
